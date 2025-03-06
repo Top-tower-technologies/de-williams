6f42df89032050dc842f438eb212cea7ced5de36
thebrainiac0204@gmail.com <toptowertechnologies@gmail.com>
2025-03-06 20:16:31 +0100

    edited

--- de-williams/script.js
+++ de-williams/script.js
@@ -33,6 +33,11 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
+
+
+
+
+
     // typing text animation script
     var typed = new Typed(".typing", {
         strings: ["Hotel", "Lounge", "Spa", "Gym", "Service Apartment"],
@@ -40,6 +45,16 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
+
+
+
+
+
+
+
+
+
+    
 
     var typed = new Typed(".typing-2", {
         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
